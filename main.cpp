f5c2810aad300eef0b8950dd868fac5286eeb40e
Radu <129198588+raduadr1an@users.noreply.github.com>
2024-12-29 23:08:03 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ class SortingVisualizer
     static constexpr int WINDOW_HEIGHT = 720;
     static constexpr int ARRAY_SIZE = 160;
     static constexpr int RECTANGLE_WIDTH = 8;
+    static constexpr int DELAY_TIME = 5;
     static constexpr const char *WINDOW_NAME = "Sorting Visualizer";
 
     SDL_Window *window;
@@ -121,21 +122,207 @@ class SortingVisualizer
             SDL_Rect rect = {i * RECTANGLE_WIDTH, WINDOW_HEIGHT - array[i], RECTANGLE_WIDTH - 1, array[i]};
 
             if (i == compareIndex || i == swapIndex)
-            {
                 SDL_SetRenderDrawColor(renderer, 255, 50, 50, 255); // red - comparing
-            }
             else if (i == minIndex)
-            {
                 SDL_SetRenderDrawColor(renderer, 50, 255, 50, 255); // green - minimum
-            }
             else
-            {
                 SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255); // white - normal
-            }
 
             SDL_RenderFillRect(renderer, &rect);
         }
         SDL_RenderPresent(renderer);
+    }
+
+    void insertionSort()
+    {
+        sorting = true;
+        for (int i = 1; i < ARRAY_SIZE && !shouldStopSorting; i++)
+        {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key && !shouldStopSorting)
+            {
+                checkEvents();
+                array[j + 1] = array[j];
+                visualize(j + 1, i, j);
+                SDL_Delay(DELAY_TIME); // Use consistent delay
+                j--;
+            }
+            array[j + 1] = key;
+            visualize(j + 1, i);
+            SDL_Delay(DELAY_TIME); // Use consistent delay
+        }
+        sorting = false;
+        if (!shouldStopSorting)
+            completionAnimation();
+    }
+
+    void merge(int left, int mid, int right)
+    {
+        std::vector<int> temp(right - left + 1);
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right && !shouldStopSorting)
+        {
+            checkEvents();
+            visualize(i, j);
+            SDL_Delay(DELAY_TIME); // Use consistent delay
+
+            if (array[i] <= array[j])
+                temp[k++] = array[i++];
+            else
+                temp[k++] = array[j++];
+        }
+
+        while (i <= mid && !shouldStopSorting)
+        {
+            checkEvents();
+            visualize(i, -1);
+            temp[k++] = array[i++];
+            SDL_Delay(DELAY_TIME); // Use consistent delay
+        }
+
+        while (j <= right && !shouldStopSorting)
+        {
+            checkEvents();
+            visualize(j, -1);
+            temp[k++] = array[j++];
+            SDL_Delay(DELAY_TIME); // Use consistent delay
+        }
+
+        for (i = 0; i < k && !shouldStopSorting; i++)
+        {
+            array[left + i] = temp[i];
+            visualize(left + i, -1);
+            SDL_Delay(DELAY_TIME); // Use consistent delay
+        }
+    }
+
+    void mergeSort(int left, int right)
+    {
+        if (left < right && !shouldStopSorting)
+        {
+            int mid = left + (right - left) / 2;
+            mergeSort(left, mid);
+            mergeSort(mid + 1, right);
+            merge(left, mid, right);
+        }
+    }
+
+    void mergeSortWrapper()
+    {
+        sorting = true;
+        mergeSort(0, ARRAY_SIZE - 1);
+        sorting = false;
+        if (!shouldStopSorting)
+            completionAnimation();
+    }
+
+    int partition(int low, int high)
+    {
+        int pivot = array[high];
+        int i = low - 1;
+
+        for (int j = low; j < high && !shouldStopSorting; j++)
+        {
+            checkEvents();
+            visualize(j, high, i + 1);
+            SDL_Delay(DELAY_TIME); // Use consistent delay
+
+            if (array[j] <= pivot)
+            {
+                i++;
+                std::swap(array[i], array[j]);
+            }
+        }
+        std::swap(array[i + 1], array[high]);
+        return i + 1;
+    }
+
+    void quickSort(int low, int high)
+    {
+        if (low < high && !shouldStopSorting)
+        {
+            int pi = partition(low, high);
+            quickSort(low, pi - 1);
+            quickSort(pi + 1, high);
+        }
+    }
+
+    void quickSortWrapper()
+    {
+        sorting = true;
+        quickSort(0, ARRAY_SIZE - 1);
+        sorting = false;
+        if (!shouldStopSorting)
+            completionAnimation();
+    }
+
+    void heapify(int n, int i)
+    {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && array[left] > array[largest])
+            largest = left;
+
+        if (right < n && array[right] > array[largest])
+            largest = right;
+
+        if (largest != i && !shouldStopSorting)
+        {
+            std::swap(array[i], array[largest]);
+            visualize(i, largest);
+            SDL_Delay(DELAY_TIME); // Use consistent delay
+            heapify(n, largest);
+        }
+    }
+
+    void heapSort()
+    {
+        sorting = true;
+
+        for (int i = ARRAY_SIZE / 2 - 1; i >= 0 && !shouldStopSorting; i--)
+            heapify(ARRAY_SIZE, i);
+
+        for (int i = ARRAY_SIZE - 1; i > 0 && !shouldStopSorting; i--)
+        {
+            checkEvents();
+            std::swap(array[0], array[i]);
+            visualize(0, i);
+            SDL_Delay(DELAY_TIME); // Use consistent delay
+            heapify(i, 0);
+        }
+
+        sorting = false;
+        if (!shouldStopSorting)
+            completionAnimation();
+    }
+
+    void shellSort()
+    {
+        sorting = true;
+        for (int gap = ARRAY_SIZE / 2; gap > 0 && !shouldStopSorting; gap /= 2)
+        {
+            for (int i = gap; i < ARRAY_SIZE && !shouldStopSorting; i++)
+            {
+                int temp = array[i];
+                int j;
+                for (j = i; j >= gap && array[j - gap] > temp && !shouldStopSorting; j -= gap)
+                {
+                    checkEvents();
+                    array[j] = array[j - gap];
+                    visualize(j, j - gap, i);
+                    SDL_Delay(DELAY_TIME); // Use consistent delay
+                }
+                array[j] = temp;
+            }
+        }
+        sorting = false;
+        if (!shouldStopSorting)
+            completionAnimation();
     }
 
     void bubbleSort()
@@ -156,7 +343,7 @@ class SortingVisualizer
                 {
                     visualize(j, j + 1);
                 }
-                SDL_Delay(5);
+                SDL_Delay(DELAY_TIME); // Use consistent delay
             }
         }
         sorting = false;
@@ -178,14 +365,14 @@ class SortingVisualizer
                     minIndex = j;
                 }
                 visualize(j, minIndex, i);
-                SDL_Delay(5);
+                SDL_Delay(DELAY_TIME); // Use consistent delay
             }
 
             if (!shouldStopSorting && minIndex != i)
             {
                 std::swap(array[minIndex], array[i]);
                 visualize(i, minIndex, i);
-                SDL_Delay(10);
+                SDL_Delay(DELAY_TIME * 2); // Slightly longer delay for swaps
             }
         }
         sorting = false;
@@ -227,6 +414,41 @@ class SortingVisualizer
                     {
                         shouldStopSorting = false;
                         bubbleSort();
+                    }
+                    break;
+                case SDLK_3:
+                    if (!sorting)
+                    {
+                        shouldStopSorting = false;
+                        insertionSort();
+                    }
+                    break;
+                case SDLK_4:
+                    if (!sorting)
+                    {
+                        shouldStopSorting = false;
+                        mergeSortWrapper();
+                    }
+                    break;
+                case SDLK_5:
+                    if (!sorting)
+                    {
+                        shouldStopSorting = false;
+                        quickSortWrapper();
+                    }
+                    break;
+                case SDLK_6:
+                    if (!sorting)
+                    {
+                        shouldStopSorting = false;
+                        heapSort();
+                    }
+                    break;
+                case SDLK_7:
+                    if (!sorting)
+                    {
+                        shouldStopSorting = false;
+                        shellSort();
                     }
                     break;
                 }
@@ -278,6 +500,11 @@ void displayMenu()
     std::cout << "| 0 - Generate new array                                        |\n";
     std::cout << "| 1 - Start Selection Sort                                      |\n";
     std::cout << "| 2 - Start Bubble Sort                                         |\n";
+    std::cout << "| 3 - Start Insertion Sort                                      |\n";
+    std::cout << "| 4 - Start Merge Sort                                          |\n";
+    std::cout << "| 5 - Start Quick Sort                                          |\n";
+    std::cout << "| 6 - Start Heap Sort                                          |\n";
+    std::cout << "| 7 - Start Shell Sort                                         |\n";
     std::cout << "| q - Return to this menu                                       |\n";
     std::cout << "+---------------------------------------------------------------+\n";
     std::cout << "| Enter 'R' to run visualizer or 'Q' to quit program           |\n";
